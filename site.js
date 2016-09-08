9eba7010bad8e7c95d73ea9587e3d0c804d7d125
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-09-08 15:50:06 -0400

    Downtown Silverspring 1.0  /site.js

---
+++ site.js
@@ -0,0 +1,7 @@
+var site_json = {
+                    "mall_name" : "Downtown Silver Spring",
+                    "default_image" : "http://assets.codecloudapp.com/sites/57d1b1536e6f642448000000/image/png/1455899596000/main_logo.png",
+                    "time_zone" : "T05:00:00Z",
+                    "newsletter_form_url" : "http://petersoncompanies.createsend.com/t/d/s/ajlit/?callback=?",
+                    "newsletter_email_field" : "cm-ajlit-ajlit"
+                }
